1ae73d68f5f17f9e234396ff3cc6deb9e5d646f7
zencode <zencode.id@gmail.com>
2022-08-31 17:56:09 +0700

    contoh submit handler

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react'
 // import Tugas7 from './components/tugas7/Tugas7';
 // import Tugas8 from './components/tugas8/Tugas8';
-// import "./App.css"
+import "./App.css"
+import AppFetch from './components/ContohFecth';
 // // import Contohstate from './components/Contohstate';
 // import Tugas9 from './components/tugas9/Tugas9';
 // import Tugas10 from './components/tugas10/Tugas10';
-import Tugas11 from './components/tugas11/Tugas11';
+// import Tugas11 from './components/tugas11/Tugas11';
 
 function App (props) {
   return (
     <div className='container'>
-     {/* <Tugas7/>
+      {/* <Tugas7/>
     <Tugas8 name="sarah" email="subkh4n@gmail.com" batch="32"/> 
     <Contohstate/>
     <Tugas9/>
-    <Tugas10/>  */}
-    <Tugas11/>
+    <Tugas10/>  
+    <Tugas11/> */}
+    <AppFetch/>
     </div>
   )
 }
--- src/components/ContohFecth.js
+++ src/components/ContohFecth.js
@@ -1,40 +1,206 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState,useEffect } from 'react'
 import axios from 'axios'
 
-const ContohFecth = () => {
+const AppFetch = () => {
+
+  //materi fetching data
   const [data, setData] = useState(null)
 
+  //materi create data
+  const [input, setInput] = useState(
+    {
+      name: ""
+    }
+  )
+
+  // const ContohFecth = () => {
+//   const [data, setData] = useState(null)
+//   // handle input state
+//   const [input, setInput] = useState(
+//     {
+//       name:""
+//     }
+//   )
+
+// ==================================
+  //indikator
+  const [fetchStatus, setFetchStatus] = useState(true)
+
   useEffect(() => {
-    axios.get("https://backendexample.sanbercloud.com/api/contestants")
+    //fetch data dengan kondisi
+    if (fetchStatus === true) {
+      axios.get("http://backendexample.sanbercloud.com/api/contestants")
+        .then((res) => {
+          setData([...res.data])
+        })
+        .catch((error) => {
+        })
+      setFetchStatus(false)
+    }
+
+  }, [fetchStatus, setFetchStatus])
+
+//   const [fetchStatus, setFetchStatus] = useState(true)
+
+//   useEffect(() => {
+//     //fetch data dengan kondisi
+//     if (fetchStatus === true) {
+//       axios.get("http://backendexample.sanbercloud.com/api/contestants")
+//         .then((res) => {
+//           setData([...res.data])
+//         })
+//         .catch((error) => {
+//         })
+//         // supaya tidak berulang
+//       setFetchStatus(false)
+//     }
+
+//   }, [fetchStatus, setFetchStatus])
+
+//=================================
+
+  //handling input
+  const handleInput = (event) => {
+    let name = event.target.name
+    let value = event.target.value
+
+    if (name === "name") {
+      setInput({ ...input, name: value })
+    }
+  }
+
+//   const handleInput = (event) => {
+//       // console.log(event.target)
+//     let name = event.target.name
+//     let value = event.target.name
+
+//     if(name === "name"){
+//       setInput({...input, name:value})
+//     } //else if(name === "hobby"){
+//      // setInput({...input,hobby:value})
+//    // }
+//   }
+//======================================
+
+  //handling submit
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    let {
+      name
+    } = input
+
+    //create data
+    axios.post('https://backendexample.sanbercloud.com/api/contestants', { name })
       .then((res) => {
-        setData([...res.data])
+        console.log(res)
+        setFetchStatus(true)
       })
-      .catch((error) => {
-      })
-  }, [])
 
-  console.log(data)
+    //clear input setelah create data
+    setInput(
+      {
+        name: ""
+      }
+    )
+  }
 
+//   //handling submit
+//   const handleSubmit = (event) => {
+//     event.preventDefault()
+
+//     let {
+//       name
+//     } = input
+
+//     //create data
+//     axios.post('https://backendexample.sanbercloud.com/api/contestants', { name })
+//       .then((res) => {
+//         console.log(res)
+//         setFetchStatus(true)
+//       })
+
+//     //clear input setelah create data
+//     setInput(
+//       {
+//         name: ""
+//       }
+//     )
+//   }
+
+//================================================
   return (
+
     <>
       <div>
         <ul>
-         
-          { data !== null && data.map((res) => {
+          {data !== null && data.map((res) => {
             return (
               <>
                 <li>  {res.name} </li>
               </>
             )
           })}
-
         </ul>
       </div>
-    </>
-  )
+{/* 
+//   return (
+//     <>
+//       <div>
+//         <ul>    
+//           { data !== null && data.map((res) => {
+//             return (
+//               <>
+//                 <li>  {res.name} </li>
+//               </>
+//             )
+//           })}
+//         </ul>
+//       </div> 
+====================================*/}
 
+
+      <p>FORM DATA</p>
+
+      {/* form data */}
+      <form onSubmit={handleSubmit}>
+        <span>Nama : </span>
+        <input onChange={handleInput} value={input.name} name='name' />
+        <input type={'submit'} />
+      </form>
+    </>
+
+  )
 }
 
+export default AppFetch
 
-export default ContohFecth
+
+
+
+
+
+
+
+
+
+
+
+//       {/* make handle input */}
+//       <p>Belajar handling input :</p>
+//         <form onSubmit={handleSubmit}>
+//           <span>nama : </span>
+//           <input onChange={handleInput} value={input.name} name='name' />
+//           <br/>
+//           {/* <input onChange={HandleInput} value={input.name} type="text" name='hobby' /> */}
+//           <br/>
+//           <input type={'submit'}/>
+//         </form>
+
+//     </>
+//   )
+
+// }
+
+
+// export default ContohFecth
